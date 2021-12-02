772de1c4ff3f5e7004f0fdcfe8991a8db60d96d9
biggiyoh <93907479+biggiyoh@users.noreply.github.com>
2021-12-02 21:07:08 +0100

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,122 @@
+const basePath = process.cwd();
+const { MODE } = require(`${basePath}/constants/blend_mode.js`);
+const { NETWORK } = require(`${basePath}/constants/network.js`);
+
+const network = NETWORK.sol;
+
+// General metadata for Ethereum
+const namePrefix = "Rig_Holder";
+const description = "Own a Open Mining runing Rig part and Exclusive VIP advantages !";
+const baseUri = "ipfs://QmcdR6UCBSuinjR9KgByAejZNGXYjbNQVmpfMedv9X7ZCi";
+
+const solanaMetadata = {
+  symbol: "GPU",
+  seller_fee_basis_points: 200, // Definir les royalties de revente 1000 = 10%
+  external_url: "https://OpenMining.com", // definir le lien du site
+  creators: [
+    {
+      address: "B3tmFq7SwnhFPegtNqV7jsWSod6YSEM5vgu5xVFec7Hh",
+      share: 100,
+    },
+  ],
+};
+
+// If you have selected Solana then the collection starts from 0 automatically
+const layerConfigurations = [
+  {
+    growEditionSizeTo: 20,
+    layersOrder: [
+      { name: "Background" },
+      { name: "Eyeball" },
+      { name: "Eye color" },
+      { name: "Iris" },
+      { name: "Shine" },
+      { name: "Bottom lid" },
+      { name: "Top lid" },
+    ],
+  },
+];
+
+const shuffleLayerConfigurations = false;
+
+const debugLogs = false;
+
+const format = {
+  width: 1000,
+  height: 1000,
+  smoothing: false,
+};
+
+const gif = {
+  export: false,
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+};
+
+const text = {
+  only: false,
+  color: "#ffffff",
+  size: 20,
+  xGap: 40,
+  yGap: 40,
+  align: "left",
+  baseline: "top",
+  weight: "regular",
+  family: "Courier",
+  spacer: " => ",
+};
+
+const pixelFormat = {
+  ratio: 2 / 128,
+};
+
+const background = {
+  generate: true,
+  brightness: "80%",
+  static: false,
+  default: "#000000",
+};
+
+const extraMetadata = {};
+
+const rarityDelimiter = "#";
+
+const uniqueDnaTorrance = 10000;
+
+const preview = {
+  thumbPerRow: 5,
+  thumbWidth: 50,
+  imageRatio: format.height / format.width,
+  imageName: "preview.png",
+};
+
+const preview_gif = {
+  numberOfImages: 5,
+  order: "ASC", // ASC, DESC, MIXED
+  repeat: 0,
+  quality: 100,
+  delay: 500,
+  imageName: "preview.gif",
+};
+
+module.exports = {
+  format,
+  baseUri,
+  description,
+  background,
+  uniqueDnaTorrance,
+  layerConfigurations,
+  rarityDelimiter,
+  preview,
+  shuffleLayerConfigurations,
+  debugLogs,
+  extraMetadata,
+  pixelFormat,
+  text,
+  namePrefix,
+  network,
+  solanaMetadata,
+  gif,
+  preview_gif,
+};
